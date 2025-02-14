2dae1d0f523bf58fb9d80bd1b48917a4dbad8920
ruzal <f0restName@email.com>
2025-02-14 11:42:59 +0300

    Changed dim_products to fct_products, updated readme and added images

--- models/mart/fct_products.sql
+++ models/mart/fct_products.sql
@@ -12,6 +12,9 @@ orders_with_returns as (
         category,
         subcategory,
         quantity,
+        sales,
+        profit,
+        discount,
         coalesce(returned, 'No') as returned
     from orders left join returns using(order_id)
 ),
@@ -27,7 +30,10 @@ products_orders as (
     select 
         product_id,
         sum(case when returned = 'No' then quantity else 0 end) as product_sold_count,
-        sum(case when returned = 'Yes' then quantity else 0 end) as product_returned_count
+        sum(case when returned = 'Yes' then quantity else 0 end) as product_returned_count,
+        sum(sales) as sales,
+        sum(profit) as profit,
+        round(avg(discount), 4) as avg_discount
     from orders_with_returns
     group by 1
 ),
@@ -38,7 +44,10 @@ final as (
         category,
         subcategory,
         coalesce(product_sold_count, 0) as product_sold_count,
-        coalesce(product_returned_count, 0) as product_returned_count
+        coalesce(product_returned_count, 0) as product_returned_count,
+        coalesce(sales, 0) as sales,
+        coalesce(profit, 0) as profit,
+        coalesce(avg_discount, 0) as avg_discount
     from products left join products_orders using(product_id)
 )
 select 
@@ -47,5 +56,8 @@ select
     category,
     subcategory,
     product_sold_count,
-    product_returned_count
+    product_returned_count,
+    sales,
+    profit,
+    avg_discount
 from final
